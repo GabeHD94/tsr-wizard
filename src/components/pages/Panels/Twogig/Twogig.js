59705bb6a569d8e58c77a6def024a23197a6a121
GabeHD94 <gabehd94@gmail.com>
2021-02-18 17:08:44 -0700

    2GIG panel sidebar has been made without links yet

--- src/components/pages/Panels/Twogig/Twogig.js
+++ src/components/pages/Panels/Twogig/Twogig.js
@@ -1,9 +1,84 @@
 import React from 'react'
+import './Twogig.css'
 
 function Twogig() {
+
+
+
     return (
-        <div>
-            <h1>hi</h1>
+        <div className='twogig-wrapper'>
+            <h1>2GIG Panel</h1>
+            <ul className='bu-wrapper'>
+                <h2>Basic Usage</h2>
+                    <li>
+                        Arm/Disarm
+                    </li>
+                    <li>
+                        Trouble Beeps
+                    </li>
+                    <li>
+                        Emergency Buttons
+                    </li>
+                <h2>Toolbox</h2>
+                    <li>
+                        Add User Codes
+                    </li>
+                    <li>
+                        Change/Remove User Code
+                    </li>
+                    <li>
+                        Brightness
+                    </li>
+                    <li>
+                        Backlight Timeout
+                    </li>
+                    <li>
+                        Clean Screen
+                    </li>
+                    <li>
+                        Calibrate Touch Screen
+                    </li>
+
+                    <li>
+                        Date Settings
+                    </li>
+                    <li>
+                        Time Settings
+                    </li>
+                    <li>
+                        Volume
+                    </li>
+                    <li>
+                        Chime Setup
+                    </li>
+                    <li>
+                        System History
+                    </li>
+                    <li>
+                        System Info
+                    </li>
+                    <li>
+                        Cell Test
+                    </li>
+                    <li>
+                        Reset Panel
+                    </li>
+                <h2>Panel Issues</h2>
+                    <li>
+                        Panel Communication Issuse
+                    </li>
+                    <li>
+                        Power Faliure
+                    </li>
+                    <li>
+                        Panel Low Battery
+                    </li>
+                <h2>Other</h2>
+                    <li>
+                        Programming Questions
+                    </li>
+            </ul>
+
         </div>
     )
 }
